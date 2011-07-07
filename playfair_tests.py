5cc8a3d7e1e3028b5a409b947cd42f8f68a2f923
Kyle Isom <coder@kyleisom.net>
2011-07-06 18:30:32 -0600

    adding playfair tests

---
+++ playfair_tests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: kyle isom <coder@kyleisom.net>
+# license: ISC / public domain dual-licensed
+#
+# unit tests for the playfair implementation
+
+import playfair
+import unittest
+
+class PlayFairTests(unittest.TestCase):
+
+    def test_playfair(self):
+        passphrase  = "President Obama's visit to a Chrysler plant in Toledo, "
+        passphrase += "Ohio, on Friday was the culmination of a campaign to "
+        passphrase += "portray the auto bailouts as a brilliant success with "
+        passphrase += "no unpleasant side effects."
+
+        key = "Pre5si9d4nt Ob2a1mvoCh8ylpTFwc3uf6g7.ABDEGHIJKLMNQRSUVWXYZj0kqxz"
+
+        crypter = playfair.PlayFair()
+        
+
+        self.assertEquals(crypter.make_key(passphrase), key)
+
+    
+    
+if __name__ == '__main__':
+    suite   = unittest.TestSuite()
+    loader  = unittest.TestLoader()
+    suite.addTests(loader.loadTestsFromTestCase(PlayFairTests))
+    unittest.TextTestRunner(verbosity = 2).run(suite)
