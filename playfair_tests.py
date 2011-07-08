7bf49249eaa5dd840e0ccb4757b95d8a30d12e61
Kyle Isom <coder@kyleisom.net>
2011-07-08 18:44:19 -0400

    adding tests to test 5x5 grid
    the examples on the programming praxis site use a 5x5 grid, so i'm adding
    code to test for that.

--- playfair_tests.py
+++ playfair_tests.py
@@ -10,6 +10,19 @@ import unittest
 
 class PlayFairTests(unittest.TestCase):
 
+    def test_playfair5(self):
+        P = playfair.PlayFair(playfair.DEF_KEYSPACE5, cols = 5, rows = 5)
+        message = 'PROGRAMMING PRAXIS' 
+        key = P.make_key( 'PLAYFAIR ')
+        ct  = P.encrypt( 'PROGRAMMING PRAXIS', key )
+        expected_ct = 'LIVOBLKZEDOELIYWCN'
+        pt  = P.decrypt(ct, key)
+        expected_pt = 'PROGRAMXMINGPRAXIS'
+
+        self.assertEquals( len(ct), len(expected_ct) )
+        self.assertEquals( ct, expected_ct )
+        self.assertEquals( pt, expected_pt )
+
     def test_playfair(self):
         passphrase  = "President Obama's visit to a Chrysler plant in Toledo, "
         passphrase += "Ohio, on Friday was the culmination of a campaign to "
@@ -17,9 +30,8 @@ class PlayFairTests(unittest.TestCase):
         passphrase += "no unpleasant side effects."
 
         key = "Pre5si9d4nt Ob2a1mvoCh8ylpTFwc3uf6g7.ABDEGHIJKLMNQRSUVWXYZj0kqxz"
-
-        crypter = playfair.PlayFair()
         
+        crypter = playfair.PlayFair()
 
         self.assertEquals(crypter.make_key(passphrase), key)
 
