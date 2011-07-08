04cf526ac3c88e6b4263f48dc2eae06a1f823c9c
Kyle Isom <coder@kyleisom.net>
2011-07-08 18:45:55 -0400

    add 5x5 default keyspace, buxfixes and encrypt/decrypt
    the partial commit didn't go through, so i'm adding it all at once. add the
    following:
    * adding support for 5x5 grid in accordance with the examples on the
    programming praxis site.
    * adding in encryption and decryption code
    * fixed bugs in code so it passes test cases
    
    code now passes the test suite including the default test vectors for a 5x5
    grid.

--- playfair.py
+++ playfair.py
@@ -2,9 +2,11 @@
 
 import struct
 
-DEF_KEYSPACE  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-DEF_KEYSPACE += 'abcdefghijklmnopqrstuvwxyz'
-DEF_KEYSPACE += '1234567890. '
+DEF_KEYSPACE8  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+DEF_KEYSPACE8 += 'abcdefghijklmnopqrstuvwxyz'
+DEF_KEYSPACE8 += '1234567890. '
+
+DEF_KEYSPACE5 = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
 LCMAP = ''.join( [ chr(i) for i in range( ord('a'), ord('k')) ] )
 
 class Char:
@@ -24,29 +26,34 @@ class PlayFair:
     passphrase = None
     keyspace = None
 
-    get_row = lambda self, i : i / 8
-    get_col = lambda self, i : i % 8
-    get_idx = lambda self, char : self.keymap.find( char )
+    get_row = lambda self, i : i / self.GRID_Y
+    get_col = lambda self, i : i % self.GRID_X
+    get_idx = lambda self, char, key : key.find( char )
     map_lc  = lambda self, char: (LCMAP.find(char) + 1) % 10
 
-    def get_char(self, char):
-        idx = self.get_idx(char)
+    def get_char(self, char, key):
+        idx = self.get_idx(char, key)
         x = self.get_col(idx)
         y = self.get_row(idx)
 
         return Char(x, y)
         
     def get_pos(self, *args):
-        if 2 == len(args):
+        if 1 == len(args):
             x = args[0][0]
             y = args[0][1]
-        if 3 == len(args):
-            x = args[1]
-            y = args[2]
+        elif 2 == len(args):
+            x = args[0]
+            y = args[1]
+        else:
+            raise Exception('invalid number of args: %d' % len(args))
+
+        return x + (y * self.GRID_Y)
         
-    def __init__(self, keyspace = DEF_KEYSPACE, rows = 8, cols = 8):
+    def __init__(self, keyspace = DEF_KEYSPACE8, rows = 8, cols = 8):
         self.GRID_X = cols
         self.GRID_Y = rows
+        
         self.keyspace = keyspace
         
     def get_digrams(self, message):
@@ -54,7 +61,13 @@ class PlayFair:
 
         while len(message) > 1:
             first, second = message[0], message[1]
-            if first == second:
+            if not first in self.keyspace:
+                message = message[1:]
+                continue
+            elif not second in self.keyspace:
+                message = message[0] + message[2:]
+                continue
+            elif first == second:
                 second = 'X'
                 message = message[1:]
             else:
@@ -97,23 +110,51 @@ class PlayFair:
                 row += key [ self.get_char(x, y) ] + ' '
             print row
 
-    def encrypt( message, key ):
-        digrams = get_digrams(message)
+    def encrypt( self, message, key ):
+        digrams = self.get_digrams(message)
         ciphertext = ''
 
+        grid = self.GRID_X * self.GRID_Y
         for digram in digrams:
-            a, b = get_char(digram[0]), get_char(digram[1])
+            a, b = self.get_char(digram[0], key), self.get_char(digram[1], key)
+            c, d = a, b
 
             if a.y == b.y:
-                a.y += 1
-                b.y += 1
-                ciphertext += key[ get_pos( a.get() ) ]
-                ciphertext += key[ get_pos( b.get() ) ]
+                c.x += 1
+                d.x += 1
             elif a.x == b.x:
-                a.x += 1
-                b.y += 1
-                ciphertext += key[ get_pos( a.get() ) ]
-                ciphertext += key[ get_pos( b.get() ) ]
-
+                c.y += 1
+                d.y += 1
+            else:
+                c = Char( b.x, a.y )
+                d = Char( a.x, b.y )
+                    
+            ciphertext += key[ self.get_pos( c.get() ) % grid ]
+            ciphertext += key[ self.get_pos( d.get() ) % grid ]
 
         return ciphertext
+
+    def decrypt( self, ciphertext, key ):
+        digrams = self.get_digrams(ciphertext)
+        plaintext = ''
+
+        grid = self.GRID_X * self.GRID_Y
+        for digram in digrams:
+            a, b = self.get_char(digram[0], key), self.get_char(digram[1], key)
+            c, d = a, b
+
+            if a.y == b.y:
+                c.x -= 1
+                d.x -= 1
+            elif a.x == b.x:
+                c.y -= 1
+                d.y -= 1
+            else:
+                c = Char( b.x, a.y )
+                d = Char( a.x, b.y )
+                    
+            plaintext += key[ self.get_pos( c.get() ) % grid ]
+            plaintext += key[ self.get_pos( d.get() ) % grid ]
+
+        return plaintext
+        
