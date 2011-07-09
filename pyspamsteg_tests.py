78250a57085562591e68a30ce1b9016d437408c6
Kyle Isom <coder@kyleisom.net>
2011-07-08 20:28:05 -0400

    add extended test coverage

--- pyspamsteg_tests.py
+++ pyspamsteg_tests.py
@@ -25,7 +25,6 @@ class PySpamStegTests(unittest.TestCase):
 
     def test_valid_read(self):
         self.assertEqual(pyspamsteg.read(test_ct01), test_pt01)
-
     def test_invalid_read(self):
         self.assertEqual(pyspamsteg.read(test_ct02), None)
 
@@ -36,6 +35,28 @@ class PySpamStegTests(unittest.TestCase):
     def test_valid_message_build(self):
         message = pyspamsteg.create(test_pt00)
         self.assertEqual(pyspamsteg.read(message), test_pt00)
+
+    def test_build(self):
+        message = 'testing pyspamsteg'
+        passphrase = 'pyspamsteg'
+        
+        ct = pyspamsteg.create( message, passphrase )
+        pt = pyspamsteg.read( ct, passphrase )
+
+        self.assertEquals( pt, message )
+
+    def test_steg_vector(self):
+        expected_pt = 'Programming Praxis'
+        passphrase  = "President Obama's visit to a Chrysler plant in Toledo, "
+        passphrase += "Ohio, on Friday was the culmination of a campaign to "
+        passphrase += "portray the auto bailouts as a brilliant success with "
+        passphrase += "no unpleasant side effects."
+        
+        vector = open('tests/spam_example_vector').read()
+        pyspamsteg.set_charsize( 7 )
+        pt = pyspamsteg.read( vector, passphrase )
+
+        self.assertEquals( pt, expected_pt )
 
 if __name__ == '__main__':
     suite   = unittest.TestSuite()
